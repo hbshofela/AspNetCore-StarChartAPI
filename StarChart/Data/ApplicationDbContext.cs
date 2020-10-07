fc03cd0c1ce9172878a9bee9b3bb41518f817fb5
akeem <hbshofela@lagetronix.com>
2020-10-07 16:52:29 +0100

    commit

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -10,6 +10,6 @@ namespace StarChart.Data
             
         }
 
-        DbSet<CelestialObject> CelestialObjects { get; set; }
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
     }
 }
